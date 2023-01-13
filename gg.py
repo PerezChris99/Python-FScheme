71d1b38ce3ac1abc42a68c25977a6d49b321f48d
perezchris3599 <kweeziperez712@gmail.com>
2023-01-13 15:37:52 +0300

    conditions

--- gg.py
+++ gg.py
@@ -16,3 +16,10 @@ while number_of_guesses < 5:
         print('Your guess is too high')
     if guess == number:
         break
+
+if guess == number:
+    print('You have guessed the number in ' + str(number_of_guesses) + 'tries!')
+else:
+    print('You did not guess the number, The number was ' + str(number))
+
+    
