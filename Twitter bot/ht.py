5a444708647f9dde1497c4212527f6832150e0ed
perezchris3599 <kweeziperez712@gmail.com>
2023-01-13 16:17:22 +0300

    methods for tweets and users timeline

--- Twitter bot/ht.py
+++ Twitter bot/ht.py
@@ -8,3 +8,13 @@ api = tweepy.API(auth)
 
 api.update_status("Hello Tweepy")
 
+#methods for users timeline
+timeline = api.home_timeline()
+for tweet in timeline:
+    print(f"{tweet.user.name} said {tweet.text}")
+
+#method for tweets
+api.update_status("Test tweet from tweepy python")
+
+
+
